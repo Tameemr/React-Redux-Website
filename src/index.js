8ada3cabd8b28436496af9936d97dcbbcd154d1e
Tameem <tameem_r@hotmail.com>
2021-05-24 13:04:43 +0300

    Fetching other data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// REDUX SETUP
+
+//REDUX SETUP
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
